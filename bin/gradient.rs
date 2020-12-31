ae9243217872504f960585f37c7b84369898f485
Steve Jenson <stevej@fruitless.org>
2020-12-31 10:01:03 -0800

    first commit, just a Vec3 and an example gradient output

---
+++ bin/gradient.rs
@@ -0,0 +1,18 @@
+use steveray::vec3::Vec3;
+
+fn main() {
+    let nx = 200;
+    let ny = 100;
+
+    println!("P3\n{} {}\n255", nx, ny);
+    for j in (0..(ny -1)).rev() {
+        for i in 0..nx {
+            let vec = Vec3{x:(i as f64)/(nx as f64), y:(j as f64)/(ny as f64), z:0.2};
+            let ir = (255.9 * vec.x()) as i32;
+            let ig = (255.9 * vec.y()) as i32;
+            let ib = (255.9 * vec.z()) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
---
+++ src/vec3.rs
@@ -0,0 +1,162 @@
+use std::ops::{Add, Sub, Mul, AddAssign, SubAssign, MulAssign, DivAssign};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    pub fn x(&self) -> &f64 { return &self.x }
+    pub fn y(&self) -> &f64 { return &self.y }
+    pub fn z(&self) -> &f64 { return &self.z }
+    pub fn r(&self) -> &f64 { return &self.x }
+    pub fn g(&self) -> &f64 { return &self.y }
+    pub fn b(&self) -> &f64 { return &self.z }
+
+    pub fn squared_length(&self) -> f64 {
+        return self.x * self.x  + self.y * self.y + self.z * self.z;
+    }
+
+    pub fn length(&self) -> f64 {
+        return self.squared_length().sqrt();
+    }
+
+    pub fn dot(&self, other: Self) -> f64 {
+        return self.r() * other.r() + self.g() * other.g() + self.b() * other.b();
+    }
+
+    pub fn cross(&self, other: Self) -> Self {
+        Self {
+            x: self.y * other.z - self.z * other.y,
+            y: -(self.x * other.z - self.z * other.x),
+            z: self.x * other.y  - self.y * other.x
+        }
+    }
+}
+
+// a + b
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+// a - b
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+// a += b
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+// a -= b
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+// a * b
+impl Mul<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+// a *= b
+impl MulAssign<Vec3> for Vec3 {
+    fn mul_assign(&mut self, other: Vec3) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+// a /= b
+impl DivAssign<Vec3> for Vec3 {
+    fn div_assign(&mut self, other: Vec3) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vec3::Vec3;
+    #[test]
+    fn dot_product() {
+        assert_eq!(Vec3{x:0.0,y:0.0,z:0.0}.dot(Vec3{x:0.0,y:0.0,z:0.0}), 0.0);
+        assert_eq!(Vec3{x:1.0,y:2.0,z:3.0}.dot(Vec3{x:4.0,y:5.0,z:6.0}), 32.0);
+    }
+
+    #[test]
+    fn cross_product() {
+        assert_eq!(Vec3{x:0.0,y:0.0,z:0.0}.cross(Vec3{x:0.0,y:0.0,z:0.0}), Vec3{x:0.0,y:0.0,z:0.0});
+        assert_eq!(Vec3{x:1.0,y:2.0,z:3.0}.cross(Vec3{x:4.0,y:5.0,z:6.0}), Vec3{x:-3.0,y:6.0,z:-3.0});
+    }
+
+    #[test]
+    fn test_length() {
+        assert_eq!(Vec3{x:1.0, y:2.0, z: 3.0}.length(), 3.7416573867739413);
+    }
+
+    #[test]
+    fn add_vecs() {
+        assert_eq!(Vec3{x:1.0, y:2.0, z:3.0} + Vec3{x: 1.0, y: 2.0, z: 3.0}, Vec3{x:2.0, y:4.0, z:6.0});
+        let mut vec123 = Vec3{x:1.0, y:2.0, z:3.0};
+        vec123 += Vec3{x: 1.0, y: 2.0, z: 3.0};
+        assert_eq!(vec123 , Vec3{x:2.0, y:4.0, z:6.0})
+    }
+    #[test]
+    fn subtract_vecs() {
+        assert_eq!(Vec3{x:1.0, y:2.0, z:3.0} - Vec3{x:1.0, y:2.0, z:3.0}, Vec3{x:0.0, y:0.0, z:0.0});
+        let mut vec123 = Vec3{x:1.0, y:2.0, z:3.0};
+        let vec123_clone = vec123.clone();
+        vec123 -= vec123_clone;
+        assert_eq!(vec123, Vec3{x:0.0, y:0.0, z:0.0});
+    }
+
+    #[test]
+    fn multiply_vecs() {
+        assert_eq!(Vec3{x:1.0, y:2.0, z:3.0} * Vec3{x:2.0, y:2.0, z:2.0}, Vec3{x:2.0, y: 4.0, z: 6.0})
+    }
+}
